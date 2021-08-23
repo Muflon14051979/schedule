787257a6eafdcd0a90f703bbff031fed56266dad
Админ <muflon@yahoo.com>
2021-08-23 23:32:33 +0300

    Initial commit

---
+++ src/eu/senla/task1/Main.java
@@ -0,0 +1,22 @@
+/*
+Напишите программу, которая возвращает количество "троек" в заданной строке.
+Тройка - это символ, появляющийся три раза подряд в строке.
+ задание на внимательность*/
+package eu.senla.task1;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for ( int c = 0; c<a; c++ ) {
+            System.out.print((char) '3' + "" + (char) '3' + "" + (char) '3');
+
+        }
+
+
+    }
+}
+
+
