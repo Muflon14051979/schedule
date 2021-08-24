3e26d969216858dd3ab317b5e5967c7614e93d85
Админ <muflon@yahoo.com>
2021-08-24 12:59:43 +0300

    Initial commit

--- src/eu/senla/task2/Main.java
+++ src/eu/senla/task2/Main.java
@@ -1,4 +1,27 @@
+/*
+Напишите программу, которая возвращает сумму цифр, присутствующих в данной строке.
+Если цифр нет, возвращаемая сумма равна 0.
+ */
 package eu.senla.task2;
+import java.util.*;
 
 public class Main {
+    private static String v = "";
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        int x = a.length();
+        for (int xx = 0; xx<x; xx++) {
+            int c = 0;
+            int c1 = 3;
+            String b = a.substring(c ,c1);
+            c1 = c1 + 1;
+            System.out.println("числа " + b);
+            v = v + b;
+        }
+        System.out.println("Длинна строки " + x);
+        System.out.println("числа " + v);
+
+    }
 }
