39423c45ebf6969311c9ee91def987d42d282e6d
Админ <muflon@yahoo.com>
2021-08-25 08:37:59 +0300

    Initial commit

--- src/eu/senla/task4/Main.java
+++ src/eu/senla/task4/Main.java
@@ -2,5 +2,18 @@
 введенное пользователем. Если введено не целое число, то сообщать ему об ошибке.
  */
 package eu.senla.task4;
+import java.util.*;
 public class Main {
-}
+    public static String a;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите целое число: ");
+        if (sc.hasNextInt()){
+            int a = sc.nextInt();
+        }else {
+            System.out.println("Вы ввели не целоечисло, введите заново: ");
+        }
+
+        }
+    }
+
